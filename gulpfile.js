3c9fad3cb16bdcbfdde11c1b5e7ea8a6fce28206
April MacDonald <angusamacdonald@hotmail.com>
2018-10-21 13:40:17 -0400

    changed configuration

--- gulpfile.js
+++ gulpfile.js
@@ -3,70 +3,17 @@ var fs = require('fs');
 var gulp = require('gulp');
 var gulpAutoprefixer = require('gulp-autoprefixer');
 var path = require('path');
-var readdirRecursive = require('fs-readdir-recursive');
 var rename = require('gulp-rename');
 var sass = require('gulp-sass');
 var webpack = require('webpack');
-var webpackStream = require('webpack-stream');
 
 var config = {
-  JS_SOURCE_DIR: './source/js/composite/',
-  JS_SOURCES: [
-    './partials/**/*.js',
-    './source/js/**/*.js',
-  ],
-  JS_OUT_DIR: './dist/js/composite/',
-  JS_OPTIONS: {
-    uglify: {
-      mangle: false
-    }
-  },
-  SASS_SOURCE_DIR: './source/sass/composite/**/*.{sass,scss}',
+  SASS_SOURCE_DIR: './source/sass/main.sass',
   SASS_SOURCES: [
-    './partials/**/*.{sass,scss}',
-    './source/sass/**/*.{sass,scss}',
+    './source/sass/**/*.{sass,scss}'
   ],
   SASS_OUT_DIR: './dist/css/composite/'
 };
-
-var jsFiles = readdirRecursive(config.JS_SOURCE_DIR);
-var entry = {};
-jsFiles.forEach(function (value) {
-  if (value.endsWith('.js')) {
-    var key = value.substring(0, value.length - 3);
-    entry[key] = config.JS_SOURCE_DIR + value;
-  }
-});
-
-var webpackConfig = {
-  entry: entry,
-  mode: 'development',
-  output: {
-    path: path.resolve(__dirname, config.JS_OUT_DIR),
-    filename: '[name].min.js'
-  }
-};
-var webpackProdConfig = extend({
-  mode: 'production',
-}, webpackConfig);
-
-gulp.task('compile-js', function() {
-  return gulp.src(config.JS_SOURCES)
-      .pipe(webpackStream(
-        webpackProdConfig, webpack
-      ))
-      .pipe(gulp.dest(config.JS_OUT_DIR));
-});
-
-gulp.task('watch-js', () => {
-  webpackConfig.watch = true;
-
-  gulp.src(config.JS_SOURCES)
-    .pipe(webpackStream(
-      webpackConfig, webpack
-    ))
-    .pipe(gulp.dest(config.JS_OUT_DIR));
-});
 
 gulp.task('compile-sass', function() {
   gulp.src(config.SASS_SOURCE_DIR)
@@ -80,7 +27,7 @@ gulp.task('compile-sass', function() {
     browsers: [
       'last 1 version',
       'last 2 iOS versions'
-    ],
+    ]
   }))
   .pipe(gulp.dest(config.SASS_OUT_DIR));
 });
@@ -89,6 +36,6 @@ gulp.task('watch-sass', function() {
   gulp.watch(config.SASS_SOURCES, ['compile-sass']);
 });
 
-gulp.task('build', ['compile-js', 'compile-sass']);
-gulp.task('grow-build', ['compile-js', 'compile-sass']);
-gulp.task('default', ['watch-js', 'compile-sass', 'watch-sass']);
+gulp.task('build', ['compile-sass']);
+gulp.task('grow-build', ['compile-sass']);
+gulp.task('default', ['compile-sass', 'watch-sass']);
